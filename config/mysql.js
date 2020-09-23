f0b99f53595d9c57e4124e219bf429bfbca24103
rbansal1394 <rbansal1394@gmail.com>
2020-09-23 11:03:30 +0530

    add database config in env file

--- config/mysql.js
+++ config/mysql.js
@@ -1,21 +1,29 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
-const con = mysql.createConnection({
-	host: 'https://uat.wishhealth.in/phpmyadmin/',
-	database: 'uatwishdb',
-	user: 'readonly',
-	password: 'R3@d0n1y',
-	// host: 'localhost',
-	// user: 'root',
-	// password: 'password',
-});
+const config =
+	process.env.SERVER_TYPE === 'local'
+		? {
+				host: process.env.LOCAL_DATABASE_HOST,
+				user: process.env.LOCAL_DATABASE_USER,
+				password: process.env.LOCAL_DATABASE_PASSWORD,
+				// database: process.env.LOCAL_DATABASE_NAME,
+		  }
+		: {
+				host: process.env.LIVE_DATABASE_HOST,
+				database: process.env.LIVE_DATABASE_NAME,
+				user: process.env.LIVE_DATABASE_USER,
+				password: process.env.LIVE_DATABASE_PASSWORD,
+		  };
+
+const con = mysql.createConnection(config);
 
 con.connect((err) => {
 	if (err) {
 		console.log('Database Connection Error: ', err);
-		throw err;
+	} else {
+		console.log('Connected!');
 	}
-	console.log('Connected!');
 });
 
 let db = mysql.connection;
