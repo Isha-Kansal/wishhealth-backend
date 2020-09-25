5eef373226837fa53d4a1510bbc3c0955d4ce62c
Kapilbindal1 <Kapilbindal1@gmail.com>
2020-09-25 16:13:45 +0530

    --server setup

--- config/mysql.js
+++ config/mysql.js
@@ -1,45 +1,45 @@
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require("mysql");
+require("dotenv").config();
 
 const {
-	SERVER_ENVIRONMENT,
+  SERVER_ENVIRONMENT,
 
-	LOCAL_DATABASE_HOST,
-	LOCAL_DATABASE_USER,
-	LOCAL_DATABASE_PASSWORD,
+  LOCAL_DATABASE_HOST,
+  LOCAL_DATABASE_USER,
+  LOCAL_DATABASE_PASSWORD,
 
-	LIVE_DATABASE_HOST,
-	LIVE_DATABASE_NAME,
-	LIVE_DATABASE_USER,
-	LIVE_DATABASE_PASSWORD,
+  LIVE_DATABASE_HOST,
+  LIVE_DATABASE_NAME,
+  LIVE_DATABASE_USER,
+  LIVE_DATABASE_PASSWORD,
 } = process.env;
 
 const config =
-	SERVER_ENVIRONMENT === 'local'
-		? {
-				host: LOCAL_DATABASE_HOST,
-				user: LOCAL_DATABASE_USER,
-				password: LOCAL_DATABASE_PASSWORD,
-				// database: LOCAL_DATABASE_NAME,
-		  }
-		: {
-				host: LIVE_DATABASE_HOST,
-				database: LIVE_DATABASE_NAME,
-				user: LIVE_DATABASE_USER,
-				password: LIVE_DATABASE_PASSWORD,
-				// port: 3306,
-				// queryTimeout: 6000,
-				// connectTimeout: 60000,
-		  };
+  SERVER_ENVIRONMENT === "local"
+    ? {
+        host: LOCAL_DATABASE_HOST,
+        user: LOCAL_DATABASE_USER,
+        password: LOCAL_DATABASE_PASSWORD,
+        // database: LOCAL_DATABASE_NAME,
+      }
+    : {
+        host: LIVE_DATABASE_HOST,
+        database: LIVE_DATABASE_NAME,
+        user: LIVE_DATABASE_USER,
+        password: LIVE_DATABASE_PASSWORD,
+        // port: 3306,
+        // queryTimeout: 6000,
+        // connectTimeout: 60000,
+      };
 
-const con = mysql.createConnection(JSON.stringify(config));
+const con = mysql.createConnection(config);
 
 con.connect((err) => {
-	if (err) {
-		console.log('Database connection Error: ', err);
-	} else {
-		console.log('Successfully connected to the database.');
-	}
+  if (err) {
+    console.log("Database connection Error: ", err);
+  } else {
+    console.log("Successfully connected to the database.");
+  }
 });
 
 let db = mysql.connection;
