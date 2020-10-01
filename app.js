86bf586bd1c7498bfcaac4b76af7b09c3d4d0931
Kapilbindal1 <Kapilbindal1@gmail.com>
2020-10-01 17:39:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-access-token,Authorization,operation"
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
   );
   next();
 });
