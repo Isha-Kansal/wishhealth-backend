5749fa9f3ee295018035b00d73143bcfcb85a3c4
Kapilbindal1 <Kapilbindal1@gmail.com>
2020-11-03 17:39:06 +0530

    changes

--- models/wh_patient_doctor_bookings.js
+++ models/wh_patient_doctor_bookings.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const db = require("../config/mysql");
+const PatientUsers = require("./wh_patient_users");
 const Users = require("./wh_users");
 
 const Bookings = db.define(
@@ -132,5 +133,12 @@ const Bookings = db.define(
     timestamps: false,
   }
 );
-
+PatientUsers.hasMany(Bookings, {
+  foreignKey: "patient_id",
+  sourceKey: "id",
+  as: "patient_bookings",
+});
+Bookings.belongsTo(PatientUsers, {
+  foreignKey: "patient_id",
+});
 module.exports = Bookings;
--- models/wh_patient_users.js
+++ models/wh_patient_users.js
@@ -134,12 +134,4 @@ const PatientUsers = db.define(
   }
 );
 
-PatientUsers.hasMany(Bookings, {
-  foreignKey: "patient_id",
-  sourceKey: "id",
-  as: "patient_bookings",
-});
-Bookings.belongsTo(PatientUsers, {
-  foreignKey: "patient_id",
-});
 module.exports = PatientUsers;
