238060ec68d66c751387d6f83e45b5c732cf6237
Kapilbindal1 <Kapilbindal1@gmail.com>
2020-11-03 13:31:31 +0530

    changes

--- models/wh_patient_users.js
+++ models/wh_patient_users.js
@@ -132,10 +132,10 @@ const PatientUsers = db.define(
     timestamps: false,
   }
 );
-PatientUsers.hasMany(Bookings, {
-  foreignKey: "patient_id",
-});
-Bookings.belongsTo(PatientUsers, {
-  foreignKey: "patient_id",
-});
+// PatientUsers.hasMany(Bookings, {
+//   foreignKey: "patient_id",
+// });
+// Bookings.belongsTo(PatientUsers, {
+//   foreignKey: "patient_id",
+// });
 module.exports = PatientUsers;
