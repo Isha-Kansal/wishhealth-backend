a5a692cae123bfe346389949487cf458868791e6
Kapilbindal1 <Kapilbindal1@gmail.com>
2020-11-03 17:05:18 +0530

    Update wh_patient_users.js

--- models/wh_patient_users.js
+++ models/wh_patient_users.js
@@ -138,7 +138,7 @@ PatientUsers.hasMany(Bookings, {
   as: "patient_users",
 });
 
-// Bookings.belongsTo(PatientUsers, {
-//   foreignKey: "patient_id",
-// });
+Bookings.belongsTo(PatientUsers, {
+  foreignKey: "patient_id",
+});
 module.exports = PatientUsers;
