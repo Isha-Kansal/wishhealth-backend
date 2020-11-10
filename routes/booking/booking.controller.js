b308d607771b89d4562c35f9783b6572da08b2ba
rbansal1394 <rbansal1394@gmail.com>
2020-11-10 12:08:18 +0530

    Update booking.controller.js

--- routes/booking/booking.controller.js
+++ routes/booking/booking.controller.js
@@ -1,84 +1,84 @@
-const Sequelize = require("sequelize");
-const PatientDetails = require("../../models/wh_patient_details");
-const commonController = require("../../common/payment");
-const Bookings = require("../../models/wh_patient_doctor_bookings");
-const Doctordetails = require("../../models/wh_doctor_details");
+const Sequelize = require('sequelize');
+const PatientDetails = require('../../models/wh_patient_details');
+const commonController = require('../../common/payment');
+const Bookings = require('../../models/wh_patient_doctor_bookings');
+const Doctordetails = require('../../models/wh_doctor_details');
 
 const { Op } = Sequelize;
 module.exports = {
-  deleteBooking: async function (req, res) {
-    try {
-      const bookingData = await Bookings.findOne({
-        where: { id: req.body.booking_id },
-      });
-      const booking = JSON.parse(JSON.stringify(bookingData));
-      const patientData = await PatientDetails.findOne({
-        where: { id: booking.patient_id },
-      });
-      const doctorData = await Doctordetails.findOne({
-        where: { user_id: booking.doctor_id },
-      });
-      const doctor = JSON.parse(JSON.stringify(doctorData));
-      const patient = JSON.parse(JSON.stringify(patientData));
-      const url = `https://2factor.in/API/R1/?module=TRANS_SMS&apikey=257e040b-f32f-11e8-a895-0200cd936042&to=${doctor.phone}&from=WishPL&templatename=DeleteAppointment&var1=${booking.date}&var2=${booking.time}&var3=${patient.name}`;
-      const session = await commonController.sendOtp(url);
-      await Bookings.destroy({ where: { id: req.body.booking_id } });
-      return res.status(200).json({
-        message: "Deleted Successfully",
-      });
-    } catch (err) {
-      console.log(err, "err");
-      return res.status(500).json({
-        message: "Something Went Wrong",
-      });
-    }
-  },
-  updateBooking: async function (req, res) {
-    try {
-      const bookingData = await Bookings.findOne({
-        where: { id: req.body.booking_id },
-      });
-      const booking = JSON.parse(JSON.stringify(bookingData));
-      const patientData = await PatientDetails.findOne({
-        where: { id: booking.patient_id },
-      });
-      const doctorData = await Doctordetails.findOne({
-        where: { user_id: booking.doctor_id },
-      });
-      const doctor = JSON.parse(JSON.stringify(doctorData));
-      const patient = JSON.parse(JSON.stringify(patientData));
-      const url = `https://2factor.in/API/R1/?module=TRANS_SMS&apikey=257e040b-f32f-11e8-a895-0200cd936042&to=${doctor.phone}&from=WishPL&templatename=DeleteAppointment&var1=${booking.date}&var2=${booking.time}&var3=${patient.name}`;
-      const session = await commonController.sendOtp(url);
-      await Bookings.update(
-        {
-          date: req.body.date,
-          time: req.body.time,
-          date2: new Date(req.body.date).setHours(0, 0, 0),
-        },
-        { where: { id: req.body.booking_id } }
-      );
-      return res.status(200).json({
-        message: "Deleted Successfully",
-      });
-    } catch (err) {
-      console.log(err, "err");
-      return res.status(500).json({
-        message: "Something Went Wrong",
-      });
-    }
-  },
-  appointmentDetail: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const booking = await Bookings.findOne({ where :{id} });
-      return res.status(200).json({
-        data: booking,
-      });
-    } catch (err) {
-      console.log("appointmentDetail-err", err);
-      return res.status(500).json({
-        message: "Something Went Wrong",
-      });
-    }
-  },
+	deleteBooking: async function (req, res) {
+		try {
+			const bookingData = await Bookings.findOne({
+				where: { id: req.body.booking_id },
+			});
+			const booking = JSON.parse(JSON.stringify(bookingData));
+			const patientData = await PatientDetails.findOne({
+				where: { id: booking.patient_id },
+			});
+			const doctorData = await Doctordetails.findOne({
+				where: { user_id: booking.doctor_id },
+			});
+			const doctor = JSON.parse(JSON.stringify(doctorData));
+			const patient = JSON.parse(JSON.stringify(patientData));
+			const url = `https://2factor.in/API/R1/?module=TRANS_SMS&apikey=257e040b-f32f-11e8-a895-0200cd936042&to=${doctor.phone}&from=WishPL&templatename=DeleteAppointment&var1=${booking.date}&var2=${booking.time}&var3=${patient.name}`;
+			const session = await commonController.sendOtp(url);
+			await Bookings.destroy({ where: { id: req.body.booking_id } });
+			return res.status(200).json({
+				message: 'Deleted Successfully',
+			});
+		} catch (err) {
+			console.log(err, 'err');
+			return res.status(500).json({
+				message: 'Something Went Wrong',
+			});
+		}
+	},
+	updateBooking: async function (req, res) {
+		try {
+			const bookingData = await Bookings.findOne({
+				where: { id: req.body.booking_id },
+			});
+			const booking = JSON.parse(JSON.stringify(bookingData));
+			const patientData = await PatientDetails.findOne({
+				where: { id: booking.patient_id },
+			});
+			const doctorData = await Doctordetails.findOne({
+				where: { user_id: booking.doctor_id },
+			});
+			const doctor = JSON.parse(JSON.stringify(doctorData));
+			const patient = JSON.parse(JSON.stringify(patientData));
+			const url = `https://2factor.in/API/R1/?module=TRANS_SMS&apikey=257e040b-f32f-11e8-a895-0200cd936042&to=${doctor.phone}&from=WishPL&templatename=DeleteAppointment&var1=${booking.date}&var2=${booking.time}&var3=${patient.name}`;
+			const session = await commonController.sendOtp(url);
+			await Bookings.update(
+				{
+					date: req.body.date,
+					time: req.body.time,
+					date2: new Date(req.body.date).setHours(0, 0, 0),
+				},
+				{ where: { id: req.body.booking_id } }
+			);
+			return res.status(200).json({
+				message: 'Deleted Successfully',
+			});
+		} catch (err) {
+			console.log(err, 'err');
+			return res.status(500).json({
+				message: 'Something Went Wrong',
+			});
+		}
+	},
+	appointmentDetail: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const booking = await Bookings.findOne({ where: { id } });
+			return res.status(200).json({
+				data: booking,
+			});
+		} catch (err) {
+			console.log('appointmentDetail-err', err);
+			return res.status(500).json({
+				message: 'Something Went Wrong',
+			});
+		}
+	},
 };
