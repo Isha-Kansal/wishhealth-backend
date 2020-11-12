45fd072f180997eb4f44101ae0c2269b1ff7d485
Kapilbindal1 <Kapilbindal1@gmail.com>
2020-11-12 12:52:00 +0530

    Update search.controller.js

--- routes/search/search.controller.js
+++ routes/search/search.controller.js
@@ -918,16 +918,7 @@ module.exports = {
           data[found] = obj;
         }
       }
-      if (
-        data.length === 0 &&
-        doctorDetails &&
-        doctorDetails.video_consultation === 1
-      ) {
-        let obj = doctorDetails;
-        obj.videobookings = videobookings;
-        obj.video_timings = video_timings;
-        data.push(obj);
-      }
+
       // data.push(ownData);
       return res.status(200).json({
         data: data,
