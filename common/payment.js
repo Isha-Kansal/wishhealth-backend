c2ac5f3b0ef0f5a06661e034ede9f63ea5952969
Kapilbindal1 <Kapilbindal1@gmail.com>
2020-11-18 13:08:51 +0530

    Update payment.js

--- common/payment.js
+++ common/payment.js
@@ -118,7 +118,7 @@ const createQuickBlox = async function (obj) {
                 },
                 {
                   where: {
-                    user_id: obj.user_id,
+                    id: obj.user_id,
                   },
                 }
               )
