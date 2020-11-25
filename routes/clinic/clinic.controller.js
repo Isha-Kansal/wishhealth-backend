83cea6a9172653effb39166323fb451ac3e550b1
rbansal1394 <rbansal1394@gmail.com>
2020-11-25 11:47:37 +0530

    doctor clinics api

--- routes/clinic/clinic.controller.js
+++ routes/clinic/clinic.controller.js
@@ -1,22 +1,50 @@
 const Clinics = require('../../models/wh_clinic');
-const ClinicTimings = require('../../models/wh_clinic_timings');
+const ClinicImages = require('../../models/wh_clinic_images');
 const DoctorClinicTimings = require('../../models/wh_doctor_clinic_timings');
+const DoctorDetails = require('../../models/wh_doctor_details');
 const patientDoctorBookings = require('../../models/wh_patient_doctor_bookings');
 
 module.exports = {
 	doctorsAllClinics: async (req, res) => {
 		try {
 			const { id } = req.params;
-			const clinics = await Clinics.findAll({
+			const doctor = await DoctorDetails.findOne({
+				where: { user_id: id },
+				attributes: ['doc_fees', 'doc_advance_fees'],
+			});
+			const doctorFees = (doctor && JSON.parse(JSON.stringify(doctor))) || {};
+			const result = await Clinics.findAll({
 				where: { admin_id: id },
 				include: [
+					{ model: ClinicImages },
 					{ model: DoctorClinicTimings },
 					{
 						model: patientDoctorBookings,
-						attributes: ['id', 'doctor_id', 'clinic_id', 'date', 'time'],
 					},
 				],
 			});
+			const clinics = (result && JSON.parse(JSON.stringify(result))) || [];
+			const timingFields = ['id', 'clinic_id', 'doctor_id', 'day'];
+			for (const clinic of clinics) {
+				let availability = {};
+				clinic.wh_doctor_clinic_timings.forEach((timing, index) => {
+					let presentTimings = Object.keys(timing).filter((time) => {
+						if (!timingFields.includes(time) && timing[time] === '1') {
+							return time;
+						}
+					});
+					availability[index + 1] = presentTimings.join(',');
+				});
+				clinic.fees = doctorFees.doc_fees;
+				clinic.advance_fees = doctorFees.doc_advance_fees;
+				clinic.availability = availability;
+				clinic.booked_slots = clinic.wh_patient_doctor_bookings;
+				clinic.clinic_images = clinic.wh_clinic_images;
+
+				delete clinic.wh_doctor_clinic_timings;
+				delete clinic.wh_patient_doctor_bookings;
+				delete clinic.wh_clinic_images;
+			}
 			return res.status(200).json({
 				data: { response: clinics },
 			});
