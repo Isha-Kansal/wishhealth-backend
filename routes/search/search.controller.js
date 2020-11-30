1f33f6a9f13481dbaaccb5393b0b8ad7b42f9f1d
Kapilbindal1 <Kapilbindal1@gmail.com>
2020-11-30 10:28:16 +0530

    Update search.controller.js

--- routes/search/search.controller.js
+++ routes/search/search.controller.js
@@ -1283,7 +1283,7 @@ module.exports = {
           clinic_id: 1,
         },
       });
-      const video_timings = await VideoConsultation.findAll({
+      const video_timings = await VideoConsultation.findOne({
         where: {
           doctor_id: req.params.user_id,
         },
